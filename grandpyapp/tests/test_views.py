e8c62374910d7ea88ff46de505b8ad054a5c49bb
FatSin <sonicfbis@hotmail.com>
2018-02-20 13:06:52 +0100

    Strucutre of HTML index.

--- grandpyapp/tests/test_views.py
+++ grandpyapp/tests/test_views.py
@@ -1,13 +1,15 @@
 import os
 import urllib.request
 
+from .. import templates
+
 #from grandpyapp import views
 
 HOST="localhost:"
 PORT="5000" 
 INDEX_FILE="index.html"
 
-#Tests the rendering of index.html
+#Test the rendering of index.html
 def test_httpcode():
 	#updir = os.path.dirname(os.path.dirname(__file__))
 	#link = os.path.join(updir,"templates",INDEX_FILE)
@@ -16,8 +18,19 @@ def test_httpcode():
 	code = urllib.request.urlopen(link).getcode()
 	#(link)
 	#print(code)
+	#HTML code expected : 20x or 30x 
 	assert code == 200
 
 #test_httpcode()
+
+
+#Check that the data in the form is sent to the system
+def test_form_html():
+	question="Where is Brian?"
+	#send the question to the form
+	# Parcourir DOM en python ????? -> minidom ??
+	#form_return = getElementById('form').
+	assert form_return == question
 	
-#HTML code expected : 20x or 30x 
+
+	
