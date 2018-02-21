8fe1f663bf5092d89543ccc06cd3022054287c74
FatSin <sonicfbis@hotmail.com>
2018-02-21 19:10:51 +0100

    Configuration of the js parsing function.

---
+++ grandpyapp/static/test_script.js
@@ -0,0 +1,22 @@
+import {parser} from "./script.js";
+
+//Import script.js
+
+//Testing the parsing
+
+//var str = "Je veux aller au 17 rue de la paix"
+
+function test_parser() {
+	var test_str = "Je veux aller au 17 rue de la paix";
+	/*if (sc.parser(test_str)=== "17 rue paix") {
+		console.log("Le parser est OK");
+	}
+	else {
+		throw new Error("Le parser est NOK");
+	}*/
+	alert(test_str);
+	console.log("Hello");
+	//parser(test_str);
+}
+
+test_parser();
