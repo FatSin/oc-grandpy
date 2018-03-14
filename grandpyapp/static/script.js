4fc72e18fa88f31639f11f3a11a49085849dafa3
FatSin <sonicfbis@hotmail.com>
2018-03-14 18:58:47 +0100

    Modification of the site's visuals.

--- grandpyapp/static/script.js
+++ grandpyapp/static/script.js
@@ -9,23 +9,29 @@ form.addEventListener("submit",function(e) {
 
 
 	console.log("parsing starts...pour"+q);
+	
+	//Display Grandpy's "thinking faces" with timers
+	$('#grandpy').attr("src",'/static/images/gp_left.png');
 
 	
 	$.getJSON('/_parse', {q}, function(data) {
 										//Display grandpybot's answer
 										$("#answer").text(data.result);
-										
+										//$('#bubble').show();
+										$('#bubble').css('visibility', 'visible');
 										console.log(data.result);
 										
 										//Displays Google Maps iframe
 										console.log(data.lnk);
 										$('#gmaps').attr("src",data.lnk);
-										$('#gmaps').show();
+										//$('#gmaps').show();
+										('#gmaps').css('visibility', 'visible');
 										$('#link_wiki').attr("href",data.wiki);
 										$('#link_wiki').show();
 										console.log("lien wiki"+data.wiki);
 										
-										
+										//Display Grandpy's "normal face"
+										$('#grandpy').attr("src",'/static/images/gp_answers.png');
 									 }
 	)
 	
